32a286c2f478fdeceb14f83009cdf3c0800eb827
Prakhar Pant <prakhar.sag22@outlook.com>
2022-04-16 01:11:22 +0530

    added js functionality to all scientific functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
+import {useState} from 'react';
+
 function App() {
+
+  const [calc, setCalc] = useState("");
+  const [disable, setDisable] = useState(false);
+  const ops = ["^"];
+
+  const squareRoot = () => {
+    setCalc(Math.sqrt(calc));
+  }
+
+  const naturalLog = () => {
+    setCalc(Math.log(calc));
+  }
+
+  const factorial = () => {
+    let fact = 1;
+    for(let i=2; i<=calc; ++i) fact *= i;
+    setCalc(fact);
+  }
+
+  const powerFunc = () => {
+    const [a,b] = calc.split("^");
+    setCalc(Math.pow(a,b)); setDisable(false);
+  }
+
+  const updateCalc = value => {
+
+    if(
+      ops.includes(value) && calc === '' ||
+      ops.includes(value) && ops.includes(calc.slice(-1))
+    ) {
+      return;
+    }
+
+    if(calc.includes("^")) setDisable(true);
+
+    setCalc(calc + value);
+  }
+
+  const deleteLast = () => {
+    if(calc == '') return;
+    const value = calc.slice(0, -1);
+
+    setCalc(value);
+  }
+
+  const deleteAll = () => {
+    if(calc == '') return;
+
+    setCalc('');
+  }
 
   const createDigitsButton= () => {
     const digits = [];
 
     for(let i=1; i<10; ++i){
       digits.push(
-        <button key={i}>{i}</button>
+        <button onClick={() => {updateCalc(i.toString())}} key={i}>{i}</button>
       )
     }
     return digits;
@@ -15,23 +67,24 @@ function App() {
     <div className="App">
       <div className='calculator'>
         <div className='display'>
-          0
+          {calc || "0"}
         </div>
 
         <div className='operators'>
-          <button>√x</button>
-          <button>x!</button>
-          <button>ln(x)</button>
-          <button>x<sup>b</sup></button>
+          <button onClick={squareRoot}>√x</button>
+          <button onClick={factorial}>x!</button>
+          <button onClick={naturalLog}>ln(x)</button>
+          <button disabled={disable} onClick={() => {updateCalc('^')}}>x<sup>b</sup></button>
 
-          <button>DEL</button>
+          <button onClick={deleteLast}>DEL</button>
+          
         </div>
 
         <div className="digits">
           {createDigitsButton()}
-          <button>0</button>
-          <button>AC</button>
-          <button>=</button>
+          <button onClick={() => {updateCalc('0')}}>0</button>
+          <button onClick={deleteAll}>AC</button>
+          <button onClick={powerFunc}>=</button>
         </div>
       </div>
     </div>
