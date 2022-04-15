d019cb2fe64c8f8fbbc56b2c293181545a9981e1
Prakhar Pant <prakhar.sag22@outlook.com>
2022-04-15 16:02:49 +0530

    added styling to the calculator app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
       <div className='calculator'>
         <div className='display'>
-          <span>0</span>
+          0
         </div>
 
         <div className='operators'>
